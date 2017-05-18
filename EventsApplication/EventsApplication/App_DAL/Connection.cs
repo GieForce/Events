5818c0fcc9451fc2ca4c44c6eeee21dbfcf599b2
DESKTOP-U9A7HHV\GieForce <s.gies@student.fontys.nl>
2017-05-18 12:58:12 +0200

    Connection added

--- EventsApplication/EventsApplication/App_DAL/Connection.cs
+++ EventsApplication/EventsApplication/App_DAL/Connection.cs
@@ -15,8 +15,7 @@ namespace EventsApplication.App_DAL
         {
             get
             {
-                SqlConnection conn = new SqlConnection("Data Source=DESKTOP-U9A7HHV\\SQLEXPRESS;Initial Catalog=EventsDatabase;Integrated Security=True");
-
+                SqlConnection conn = new SqlConnection("Data Source = 192.168.20.22; Initial Catalog = Eyect4Events; User ID = dbi364892; Password = Password123");
                 try
                 {
                     conn.Open();
